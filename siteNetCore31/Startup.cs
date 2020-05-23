b95c623f66d0686512ef761e957eac81b3af0bd9
Wintego <38418@gmail.com>
2020-05-23 15:25:22 +0300

    Rонфигурация приложения

--- siteNetCore31/Startup.cs
+++ siteNetCore31/Startup.cs
@@ -5,15 +5,25 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using siteNetCore31.Service;
 
 namespace siteNetCore31
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            //подключаем когфиг из appsettings.json
+            Configuration.Bind("Project", new Config());
             //регистрируем сервис поддержки контроллеров и представлений (MVC)
             services.AddControllersWithViews()
                 //устанавливаем совместимость с apn.net core 3.0
