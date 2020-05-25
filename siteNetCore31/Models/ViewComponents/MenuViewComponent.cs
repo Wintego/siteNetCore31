8d503a833037c9c3f7b74f55da75c3cd21230cf9
Wintego <38418@gmail.com>
2020-05-25 18:32:22 +0300

    Добавлено 3 шаблона страниц Правки

---
+++ siteNetCore31/Models/ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using siteNetCore31.Domain.Repsitories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace siteNetCore31.Models.ViewComponents
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        private readonly DataManager dataManager;
+
+        public MenuViewComponent(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
+        public Task<IViewComponentResult> InvokeAsync()
+        {
+            return Task.FromResult((IViewComponentResult)View("Default", dataManager.Pages.GetPages()));
+        }
+    }
+}
---
+++ siteNetCore31/Models/ViewComponents/SidebarViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using siteNetCore31.Domain.Repsitories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace siteNetCore31.Models.ViewComponents
+{
+    public class SidebarViewComponent : ViewComponent
+    {
+        private readonly DataManager dataManager;
+
+        public SidebarViewComponent(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+
+        public Task<IViewComponentResult> InvokeAsync()
+        {
+            return Task.FromResult((IViewComponentResult)View("Default", dataManager.Services.GetServices()));
+        }
+    }
+}
