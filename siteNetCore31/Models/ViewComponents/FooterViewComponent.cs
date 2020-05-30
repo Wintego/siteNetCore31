73a076bd666bffca34d00b1af1edd53109a720c0
Wintego <38418@gmail.com>
2020-05-30 21:39:40 +0300

    Добавлен футер

---
+++ siteNetCore31/Models/ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using siteNetCore31.Domain.Repsitories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace siteNetCore31.Models.ViewComponents
+{
+    public class FooterViewComponent : ViewComponent
+    {
+        private readonly DataManager dataManager;
+
+        public FooterViewComponent(DataManager dataManager)
+        {
+            this.dataManager = dataManager;
+        }
+        public Task<IViewComponentResult> InvokeAsync()
+        {
+            return Task.FromResult((IViewComponentResult)View("Default", dataManager.Pages.GetPages()));
+        }
+    }
+}
